4275f30e01d257b0fb99ed4902ba991d3d90658d
Mateus <mateus.zichack@gmail.com>
2026-06-21 18:02:33 -0300

    estrutura de pastas, conexão com banco de dados

---
+++ banco.sql
@@ -0,0 +1,9 @@
+--CREATE DATABASE projeto_php;
+
+/*USE projeto_php;
+    CREATE TABLE jogos(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        titulo VARCHAR(100) NOT NULL,
+        genero VARCHAR(50) NOT NULL,
+        plataforma VARCHAR(50) NOT NULL ,
+        ano_lancamento INT NOT NULL);*/
