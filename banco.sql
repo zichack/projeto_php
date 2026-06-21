ea95dfef2bb049ffe759c8841f7a7facfd1eccd5
Mateus <mateus.zichack@gmail.com>
2026-06-21 18:55:27 -0300

    método para listar e cadastra jogos, ajuste conexão com banco

--- banco.sql
+++ banco.sql
@@ -7,3 +7,6 @@
         genero VARCHAR(50) NOT NULL,
         plataforma VARCHAR(50) NOT NULL ,
         ano_lancamento INT NOT NULL);*/
+
+/*INSERT INTO jogos (titulo, genero, plataforma, ano_lancamento) 
+VALUES ('Kingdom Come: Deliverance', 'RPG', 'PC', 2018);*/
--- config/Database.php
+++ config/Database.php
@@ -1,6 +1,6 @@
 <?php
 class Database {
-    private $host = "localhost";
+    private $host = "127.0.0.1";
     private $db_name = "projeto_php";
     private $username = "root";
     private $password = "";
